c495c7e5bfc23d539a86733033245939d107dca6
Jingyao Zong <zongjingyao@gmail.com>
2018-03-07 16:11:16 +0800

    Add 3 methods in batchmode.py

--- tools/batchmode/batchmode.py
+++ tools/batchmode/batchmode.py
@@ -42,11 +42,83 @@ class BatchMode():
             logger.info('Begin build with cmd: \n\t' + ' '.join(cmds))
             ret_code = subprocess.call(cmds, shell=False)
             if ret_code == 0:
-                logger.info('Done. Return code: %d.\n\tlog file: %s\n\tapk path:%s' %
+                logger.info('Done. Return code: %d.\n\tlog file: %s\n\tapk path: %s' %
                             (ret_code, args['logFile'], args['apkPath']))
             else:
                 logger.error('Failed. Return code: %d.\n\tPlease check the log: %s' %
                              (ret_code, args['logFile']))
+
+    def create_project(self, project):
+        logger.error('Create project at ' + project['projectPath'])
+        if path.exists(project['projectPath']):
+            logger.error('Project has already been existing!')
+            return
+        # create project
+        cmds = [
+            self._default['unityPath'], '-batchmode', '-quit', '-logFile',
+            project['logFile'], '-createProject', project['projectPath']
+        ]
+        logger.info('Create project with cmd: \n\t' + ' '.join(cmds))
+        ret_code = subprocess.call(cmds, shell=False)
+        if ret_code == 0:
+            logger.info('Done. Return code: %d.\n\tlog file: %s' % (ret_code, project['logFile']))
+        else:
+            logger.error('Failed. Return code: %d.\n\tPlease check the log: %s' % (ret_code, 
+                project['logFile']))
+
+    def import_package(self, project, package_path):
+        cmds = [
+            self._default['unityPath'], '-projectPath', project['projectPath'], '-logFile',
+            project['logFile'], '-importPackage', package_path)
+        ]
+        logger.info('Import package with cmd: \n\t' + ' '.join(cmds))
+        ret_code = subprocess.call(cmds, shell=False)
+        if ret_code == 0:
+            logger.info('Done. Return code: %d.\n\tlog file: %s' % (ret_code, project['logFile']))
+        else:
+            logger.error('Failed. Return code: %d.\n\tPlease check the log: %s' % (ret_code, 
+                project['logFile']))
+
+    def enable_iap(self, project):
+        project_path = self._projects[0]['projectPath']
+        # ProjectSettings.asset
+        project_settings_path = path.join(project_path, 'ProjectSettings/ProjectSettings.asset')
+        logger.info('Edit %s' % project_settings_path)
+        with open(project_settings_path, 'r+') as project_settings:
+            new_lines = []
+            for line in project_settings:
+                tmp = line.strip()
+                if tmp.startswith('cloudProjectId'):
+                    new_lines.append('  cloudProjectId: %s\n' % project['cloudProjectId'])
+                elif tmp.startswith('projectName'):
+                    new_lines.append('  projectName: %s\n' % project['projectName'])
+                elif tmp.startswith('organizationId'):
+                    new_lines.append('  organizationId: %s\n' % project['organizationId'])
+                else:
+                    new_lines.append(line)
+            project_settings.seek(0, 0)
+            project_settings.writelines(new_lines)
+
+        # UnityConnectSettings.asset
+        connect_settings_path = path.join(project_path,
+                                          'ProjectSettings/UnityConnectSettings.asset')
+        logger.info('Edit %s' % connect_settings_path)
+        with open(connect_settings_path, 'r+') as connect_settings:
+            new_lines = []
+            found = False
+            for line in connect_settings:
+                tmp = line.strip()
+                if found and tmp.startswith('m_Enabled'):
+                    new_lines.append('    m_Enabled: 1\n')
+                    continue
+                if tmp.startswith('UnityPurchasingSettings') or \
+                    tmp.startswith('UnityAnalyticsSettings'):
+                    found = True
+                    new_lines.append(line)
+                else:
+                    new_lines.append(line)
+            connect_settings.seek(0, 0)
+            connect_settings.writelines(new_lines)
 
 
 def main():
